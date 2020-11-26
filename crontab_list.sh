2f1ff544b61286cc1c807420da8e3780f52366f9
小宇 <xiaoyu@haoshengmall.com>
2020-11-26 12:57:01 +0800

    node

--- crontab_list.sh
+++ crontab_list.sh
@@ -1,7 +1,7 @@
 #喜马拉雅
 */30 8-23 * * * /usr/bin/python3 /root/xmly_speed/xmly_speed.py >> /root/xmly_speed/xmly.log 2>&1
 #qq阅读
-*/20 8-23 * * * node /root/script/qqread.js >> /root/script/log/qqread.log 2>&1
+*/20 8-23 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/qqread.js >> /root/script/log/qqread.log 2>&1
 
 
 
