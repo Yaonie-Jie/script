41cee1591b2edb216befd20143d0d0ba23cf400e
小宇 <xiaoyu@haoshengmall.com>
2020-12-10 18:28:26 +0800

    add docker yml

--- crontab_list.sh
+++ crontab_list.sh
@@ -4,6 +4,8 @@
 */11 * * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/qqread.js >> /root/script/log/qqread.log 2>&1
 #快手极速版
 30 9 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/kuaishou.js >> /root/script/log/kuaishou.log 2>&1
+#提交互助
+30 4 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/utils/shareCode.js >> /root/script/log/shareCode.log 2>&1
 
 
 
