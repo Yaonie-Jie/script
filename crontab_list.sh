ebf899d12c7404c2fd8ef39c08ddfea9f0f5e68d
小宇 <xiaoyu@haoshengmall.com>
2020-12-17 09:50:46 +0800

    1

--- crontab_list.sh
+++
@@ -1,2 +0,0 @@
-#宠汪汪邀请助力
-10 10,11 * * * node /scripts/jd_joy_run.js >> /scripts/logs/jd_joy_run.log 2>&1
---
+++ my_crontab_list.sh
@@ -0,0 +1,2 @@
+#宠汪汪邀请助力
+55 9 * * * node /scripts/jd_watch.js >> /scripts/logs/jd_watch.log 2>&1
