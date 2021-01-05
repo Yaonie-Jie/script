bd53b566038e64f020fad10cb45858cbd0bdfc2f
小宇 <xiaoyu@haoshengmall.com>
2021-01-05 18:34:11 +0800

    头条

--- crontab_list.sh
+++ crontab_list.sh
@@ -5,12 +5,14 @@
 #百度极速版
 10 8-18/2 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/baidu_speed.js >> /root/script/log/baidu_speed.log 2>&1
 #提交互助
-30 4 1,10,20 * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/utils/shareCode.js >> /root/script/log/shareCode.log 2>&1
+10,40 4 1,10,20 * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/utils/shareCode.js >> /root/script/log/shareCode.log 2>&1
 #聚看
 */30 7-22 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/jukan.js >> /root/script/log/jukan.log 2>&1
 #快手签到
 45 9 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/kuaishou.js >> /root/script/log/kuaishou.log 2>&1
 # 抖音极速版
 */20 7-22 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/dyjsb.js >> /root/script/log/dyjsb.log 2>&1
+#今日头条
+5,35 8-23 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/jrtt.js >> /root/script/log/jrtt.log 2>&1
 
 55 23 * * * rm -rf /root/script/log/*.log
--- src/dxbox.js
+++ src/dxbox.js
@@ -1,18 +1,18 @@
 let a = {
-  id: "dyjsb",
-  name: "抖音极速版",
+  id: "jrtt",
+  name: "今日头条极速版",
   keys: [
-    "signheader",
-    "signcookie",
-    "stepheader",
-    "stepkey",
-    "readheader",
+    "signurl",
+    "signkey",
+    "farmurl",
+    "farmkey",
+    "readurl",
     "readkey",
-    "signheader",
-    "signcookie2",
-    "stepheader2",
-    "stepkey2",
-    "readheader2",
+    "signurl2",
+    "signkey2",
+    "farmurl2",
+    "farmkey2",
+    "readurl2",
     "readkey2",
   ],
   author: "@ZhiYi-N",
@@ -25,45 +25,53 @@ let a = {
       desc: "0为不通知，1为全部通知",
     },
     {
-      id: "dyjsbsetting",
+      id: "jrttsetting",
       name: "当前账号",
       val: "1",
       type: "number",
       desc: "当前账号对应",
     },
     {
-      id: "dyjsbcount",
+      id: "jrttcount",
       name: "账号个数",
       val: "1",
       type: "number",
       desc: "需要几个账号，数字为几",
     },
   ],
-  repo: "https://github.com/ZhiYi-N/script/blob/master/dyjsb.js",
+  repo: "https://github.com/ZhiYi-N/script/blob/master/jrtt.js",
   icons: [
-    "https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/img/dyjsb.jpg",
-    "https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/img/dyjsb.jpg",
+    "https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/img/jrtt.jpg",
+    "https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/img/jrtt.jpg",
   ],
-  script: "https://raw.githubusercontent.com/ZhiYi-N/script/master/dyjsb.js",
+  script: "https://raw.githubusercontent.com/ZhiYi-N/script/master/jrtt.js",
   icon:
-    "https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/img/dyjsb.jpg",
+    "https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/img/jrtt.jpg",
   favIcon: "mdi-star-outline",
   favIconColor: "grey",
   datas: [
     {
-      key: "signheader",
+      key: "signurl",
       val:
         "",
     },
     {
-      key: "signcookie",
+      key: "signkey",
+      val:
+        '',
+    },
+    {
+      key: "farmurl",
       val:
         "",
     },
-    { key: "stepheader", val: "" },
-    { key: "stepkey", val: "" },
     {
-      key: "readheader",
+      key: "farmkey",
+      val:
+        '',
+    },
+    {
+      key: "readurl",
       val:
         "",
     },
@@ -72,15 +80,11 @@ let a = {
       val:
         '',
     },
-    {
-      key: "signheader",
-      val:
-        "_request_from=web&version_code=13.1.2&js_sdk_version=1.77.0.2&tma_jssdk_version=1.77.0.2&app_name=douyin_lite&app_version=13.1.2&vid=858C52D1-CF94-4E2B-9B09-1CA70C7EDEFA&device_id=70190900117&channel=App%20Store&mcc_mnc=46011&aid=2329&screen_width=828&openudid=3a9390b219f15d71b0d88308f482235ce45cd8bb&cdid=F727A44C-83EA-484A-86F0-91DF1AE40346&os_api=18&ac=WIFI&os_version=13.2.3&client_niu_ready=1&device_platform=iphone&build_number=131202&iid=1952382687127208&device_type=iPhone12,1&idfa=533BC320-CE01-4818-B7D7-9EF43517A1D0&in_sp_time=0",
-    },
-    { key: "signcookie2", val: "" },
-    { key: "stepheader2", val: "" },
-    { key: "stepkey2", val: "" },
-    { key: "readheader2", val: "" },
+    { key: "signurl2", val: "" },
+    { key: "signkey2", val: "" },
+    { key: "farmurl2", val: "" },
+    { key: "farmkey2", val: "" },
+    { key: "readurl2", val: "" },
     { key: "readkey2", val: "" },
   ],
   sessions: [],
