b12b6273db07a27eb55a93ffdfb88072118c640e
小宇 <xiaoyu@haoshengmall.com>
2021-01-05 10:45:36 +0800

    dyjsb

--- src/dxbox.js
+++ src/dxbox.js
@@ -1,9 +1,88 @@
-835990s.
-2021/01/01 12:31:27:045  <console>    [JS Console]: 
-🔔聚看点极速, 开始!
-2021/01/01 12:31:27:052  <console>    [JS Console]: Cookie:
-2021/01/01 12:31:27:053  <console>    [JS Console]: bodyVal:
-2021/01/01 12:31:27:054  <console>    [JS Console]: 
-==============📣系统通知📣==============
-聚看点极速
-获取Cookie成功
+let a = {
+  id: "dyjsb",
+  name: "抖音极速版",
+  keys: [
+    "signheader",
+    "signcookie",
+    "stepheader",
+    "stepkey",
+    "readheader",
+    "readkey",
+    "signheader",
+    "signcookie2",
+    "stepheader2",
+    "stepkey2",
+    "readheader2",
+    "readkey2",
+  ],
+  author: "@ZhiYi-N",
+  settings: [
+    {
+      id: "tz",
+      name: "静默运行",
+      val: "1",
+      type: "number",
+      desc: "0为不通知，1为全部通知",
+    },
+    {
+      id: "dyjsbsetting",
+      name: "当前账号",
+      val: "1",
+      type: "number",
+      desc: "当前账号对应",
+    },
+    {
+      id: "dyjsbcount",
+      name: "账号个数",
+      val: "1",
+      type: "number",
+      desc: "需要几个账号，数字为几",
+    },
+  ],
+  repo: "https://github.com/ZhiYi-N/script/blob/master/dyjsb.js",
+  icons: [
+    "https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/img/dyjsb.jpg",
+    "https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/img/dyjsb.jpg",
+  ],
+  script: "https://raw.githubusercontent.com/ZhiYi-N/script/master/dyjsb.js",
+  icon:
+    "https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/img/dyjsb.jpg",
+  favIcon: "mdi-star-outline",
+  favIconColor: "grey",
+  datas: [
+    {
+      key: "signheader",
+      val:
+        "",
+    },
+    {
+      key: "signcookie",
+      val:
+        "",
+    },
+    { key: "stepheader", val: "" },
+    { key: "stepkey", val: "" },
+    {
+      key: "readheader",
+      val:
+        "",
+    },
+    {
+      key: "readkey",
+      val:
+        '',
+    },
+    {
+      key: "signheader",
+      val:
+        "_request_from=web&version_code=13.1.2&js_sdk_version=1.77.0.2&tma_jssdk_version=1.77.0.2&app_name=douyin_lite&app_version=13.1.2&vid=858C52D1-CF94-4E2B-9B09-1CA70C7EDEFA&device_id=70190900117&channel=App%20Store&mcc_mnc=46011&aid=2329&screen_width=828&openudid=3a9390b219f15d71b0d88308f482235ce45cd8bb&cdid=F727A44C-83EA-484A-86F0-91DF1AE40346&os_api=18&ac=WIFI&os_version=13.2.3&client_niu_ready=1&device_platform=iphone&build_number=131202&iid=1952382687127208&device_type=iPhone12,1&idfa=533BC320-CE01-4818-B7D7-9EF43517A1D0&in_sp_time=0",
+    },
+    { key: "signcookie2", val: "" },
+    { key: "stepheader2", val: "" },
+    { key: "stepkey2", val: "" },
+    { key: "readheader2", val: "" },
+    { key: "readkey2", val: "" },
+  ],
+  sessions: [],
+  isLoaded: true,
+};
