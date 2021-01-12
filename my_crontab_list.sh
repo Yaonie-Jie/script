f2f68b969f43a7731c9595fef9ad5bac495ac207
小宇 <xiaoyu@haoshengmall.com>
2021-01-12 13:39:12 +0800

    1

--- my_crontab_list.sh
+++ my_crontab_list.sh
@@ -1,5 +1,2 @@
-#看一看
-1 7 * * * node /scripts/jd_mh.js >> /scripts/logs/jd_mh.log 2>&1
-
-#快手播放视频
-# */30 2,8,16 * * * node /scripts/xiaoyu/auto_kuaishou.js >> /scripts/logs/auto_kuaishou.log 2>&1
+#盲盒
+1 7 * * * node /scripts/xiaoyu/jd_mh.js >> /scripts/logs/jd_mh.log 2>&1
