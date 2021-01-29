4f2d892437449516f9346728c7b062c63cf85bd6
小宇 <xiaoyu@haoshengmall.com>
2021-01-29 14:38:08 +0800

    1

--- crontab_list.sh
+++ crontab_list.sh
@@ -2,6 +2,8 @@
 */30 * * * * /usr/bin/python3 /root/script/python/xmly.py >> /root/script/log/xmly.log 2>&1
 #百度极速版
 */30 6-18 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/baidu_speed.js >> /root/script/log/baidu_speed.log 2>&1
+#笑谱
+*/10 6-20 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/iboxpay.js >> /root/script/log/iboxpay.log 2>&1
 #快手签到
 45 9 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/kuaishou.js >> /root/script/log/kuaishou.log 2>&1
 #今日头条
--- src/dxbox.js
+++ src/dxbox.js
@@ -1,19 +1,20 @@
 let a = {
   Accept: "*/*",
-  "Accept-Encoding": "gzip, deflate, br",
+  "Accept-Encoding": "br, gzip, deflate",
   "Accept-Language": "zh-Hans-CN;q=1",
   Connection: "keep-alive",
   "Content-Type": "application/json; charset=utf-8",
   Host: "veishop.iboxpay.com",
   "User-Agent":
-    "VeiShop, 1.4.4 (iOS, 13.3.1, zh_CN, Apple, iPhone, 4D3E22BA-BD88-46CC-985C-C5CDD55510E5)",
+    "VeiShop, 1.4.4 (iOS, 12.4.8, zh_CN, Apple, iPhone, 9AD90C12-D759-4742-9DF3-2129622147EA)",
   "X-User-Agent":
-    "VeiShop, 1.4.4 (iOS, 13.3.1, zh_CN, Apple, iPhone, 4D3E22BA-BD88-46CC-985C-C5CDD55510E5)",
+    "VeiShop, 1.4.4 (iOS, 12.4.8, zh_CN, Apple, iPhone, 9AD90C12-D759-4742-9DF3-2129622147EA)",
   mchtNo: "100529600058887",
   shopkeeperId: "1148855820752977920",
   source: "VEISHOP_APP_IOS",
-  token: "8168b477a895436091e97f834963a388",
-  traceid: "313506639147055759361610858319253a29d88dea4b8",
+  token: "70701229964a4b2896eb0d76f1a35c4b",
+  traceid: "313504844036701470721611902111376a29d88dea4b8",
   version: "1.4.4",
 };
-console.log(JSON.stringify(a));
+
+console.log(JSON.stringify(a))
