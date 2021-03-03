b2aa106d6f1bb2af56affcdd456ca50f7771760d
小宇 <xiaoyu@haoshengmall.com>
2021-03-03 15:26:21 +0800

    1

--- crontab_list.sh
+++ crontab_list.sh
@@ -10,5 +10,7 @@
 5,35 8-23 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/jrtt.js >> /root/script/log/jrtt.log 2>&1
 #悦动族
 0,30 0-23 * * * /root/.nvm/versions/node/v14.15.1/bin/node /root/script/src/yuedongzu.js >> /root/script/log/yuedongzu.log 2>&1
+#联通签到
+3 16 * * * /usr/bin/python3 /root/script/python/unicomTask/main.py >> /root/script/log/unicomTask.log 2>&1
 
 55 23 * * * rm -rf /root/script/log/*.log
